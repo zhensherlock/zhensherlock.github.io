de0644a039e5d36c6888501fa254bdd77b429768
孙臻轩 <zhensherlock@163.com>
2017-06-06 23:09:07 +0800

    delete indexeddb

--- example/indexeddb/js/dbhelper.js
+++
@@ -1,9 +0,0 @@
-var dbHelper = {};
-/**
- * 获取数据库名称列表
- */
-dbHelper.getDatabaseNames = () => {
-    indexedDB.webkitGetDatabaseNames().onsuccess = (event) => {
-        let result = event.target.result;
-    };
-};
